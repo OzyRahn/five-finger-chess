61411d19f75c29b4028f94ce82e1c68c067adbdb
krisyang1007 <164922686+krisyang1007@users.noreply.github.com>
2024-05-14 00:37:14 +0800

    Update Gomoku.h

--- Gomoku/Gomoku/Gomoku.h
+++ Gomoku/Gomoku/Gomoku.h
@@ -1,1 +1,12 @@
-
+#ifndef Gomoku_h
+#define Gomoku_h
+class Gomoku
+{
+private:
+	char map[15][15]; // 地圖大小為15 * 15
+public:
+	Gomoku();       //地圖初始化
+	void show() const;    //顯示棋盤
+	void welcome() const;   //歡迎介面
+};
+#endif // !Gomoku_h
