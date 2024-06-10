69c29cadc8a4b28d8c5c40cda34e7fd64a32b166
krisyang1007 <164922686+krisyang1007@users.noreply.github.com>
2024-06-10 16:12:11 +0800

    Update Gomoku.h

--- Gomoku/Gomoku/Gomoku.h
+++ Gomoku/Gomoku/Gomoku.h
@@ -8,5 +8,12 @@ public:
 	Gomoku();       //地圖初始化
 	void show() const;    //顯示棋盤
 	void welcome() const;   //歡迎介面
+	bool colWin(char, int, int); //判斷直向棋子數  
+	bool rowWin(char, int, int); //判斷橫向棋子數 
+	bool obliWinl(char, int, int);  //判斷斜向棋子數
+	bool obliWin2(char, int, int); //判斷斜向棋子數
+	bool winner(char, int, int); //上述四個判斷有一個成立,及獲勝
+	bool tfSet(char, int, int);  //輸入棋子位置
+	bool mapOver();     //搜尋是否滿盤,只要有一個是空的,地圖就不為滿
 };
 #endif // !Gomoku_h
